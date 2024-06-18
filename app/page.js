7d07b3e80971d3577c377c274d4e5571d315c60e
kanishka-sahoo <kanishka.sa.hoo@gmail.com>
2024-06-18 20:10:00 +0530

    Updated main.yml

--- app/page.js
+++ app/page.js
@@ -1,113 +1,146 @@
-import Image from "next/image";
+"use client";
+
+import React, { useState, useEffect, useRef } from "react";
+import TaskCard from "./taskcard";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    const [tasks, setTasks] = useState([]);
+    const [inputValue, setInputValue] = useState("");
+    const [dateValue, setDateValue] = useState(null);
+    const [timeValue, setTimeValue] = useState(null);
+    const [editingTaskId, setEditingTaskId] = useState(null);
+    const inputRef = useRef(null);
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    useEffect(() => {
+        const storedTasks = localStorage.getItem("tasks");
+        if (storedTasks) {
+            setTasks(JSON.parse(storedTasks));
+        }
+    }, []);
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+    useEffect(() => {
+        if (editingTaskId !== null && inputRef.current) {
+            inputRef.current.focus();
+        }
+    }, [editingTaskId]);
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+    const handleInputChange = (event) => {
+        setInputValue(event.target.value);
+    };
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+    const handleDateChange = (event) => {
+        setDateValue(event.target.value);
+    };
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    const handleTimeChange = (event) => {
+        setTimeValue(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (!inputValue.trim()) {
+            return;
+        }
+        let updatedTasks;
+        if (editingTaskId !== null) {
+            updatedTasks = tasks.map((task) =>
+                task.id === editingTaskId ? { ...task, task: inputValue, date: dateValue, time: timeValue } : task
+            );
+            setEditingTaskId(null);
+        } else {
+            const newTask = {
+                id: new Date().getTime(),
+                task: inputValue,
+                completed: false,
+                date: dateValue,
+                time: timeValue
+            };
+            updatedTasks = [...tasks, newTask];
+        }
+        setTasks(updatedTasks);
+        localStorage.setItem("tasks", JSON.stringify(updatedTasks));
+        setInputValue("");
+        setDateValue("");
+        setTimeValue("");
+    };
+
+    const handleToggle = (id) => {
+        const updatedTasks = tasks.map((task) =>
+            task.id === id ? { ...task, completed: !task.completed } : task
+        );
+        setTasks(updatedTasks);
+        localStorage.setItem("tasks", JSON.stringify(updatedTasks));
+    };
+
+    const handleDelete = (id) => {
+        const updatedTasks = tasks.filter((task) => task.id !== id);
+        setTasks(updatedTasks);
+        localStorage.setItem("tasks", JSON.stringify(updatedTasks));
+    };
+
+    const handleEdit = (id, taskname, date, time) => {
+        setEditingTaskId(id);
+        setInputValue(taskname);
+        setDateValue(date);
+        setTimeValue(time);
+    };
+    const sortedTasks = tasks.sort((a, b) => {
+        if (a.date === b.date) {
+            return a.time.localeCompare(b.time);
+        } else {
+            return a.date.localeCompare(b.date);
+        }
+    });
+    return (
+        <main className="flex flex-col flex-1 min-h-screen dark:bg-gray-900 dark:text-white">
+            <div className="flex flex-col items-center flex-1 p-4 w-full lg:max-w-4xl mx-auto">
+                <h1 className="text-5xl sm:text-6xl font-bold pt-16">Task Planner</h1>
+                <form
+                    onSubmit={handleSubmit}
+                    className="flex text-white font-bold rounded mt-8 w-full flex-col md:flex-row"
+                >
+                    <input
+                        type="text"
+                        className="border border-blue-500 text-black font-bold py-4 px-4 rounded mt-4 w-full md:w-1/3 md:mr-2"
+                        placeholder="Enter task"
+                        value={inputValue}
+                        onChange={handleInputChange}
+                        ref={inputRef}
+                        autoFocus
+                    />
+                    <input
+                        type="date"
+                        className="border border-blue-500 text-black font-bold py-4 px-4 rounded mt-4 w-full md:w-1/3 md:ml-2 md:mr-2 max-h-16"
+                        value={dateValue}
+                        onChange={handleDateChange}
+                    />
+                    <input
+                        type="time"
+                        className="border border-blue-500 text-black font-bold py-4 px-4 rounded mt-4 w-full md:w-1/3 md:ml-2 md:mr-2 max-h-16"
+                        value={timeValue}
+                        onChange={handleTimeChange}
+                    />
+                    <button
+                        type="submit"
+                        className="bg-blue-500 border border-blue-500 text-white font-bold py-4 px-4 rounded mt-4 w-full md:w-1/3 md:ml-2 max-h-16"
+                    >
+                        {editingTaskId !== null ? "Update Task" : "Add Task"}
+                    </button>
+                </form>
+                {sortedTasks.map((task, index) => (
+                    <TaskCard
+                        key={index}
+                        id={index}
+                        task={task.task}
+                        date={task.date}
+                        time={task.time}
+                        completed={task.completed}
+                        onToggle={() => handleToggle(task.id)}
+                        onDelete={() => handleDelete(task.id)}
+                        onEdit={() => handleEdit(task.id, task.task, task.date, task.time)}
+                    />
+                ))}
+            </div>
+        </main>
+    );
 }
